e6a105f2790c41a941d2c7b01f689e8ad74781a3
Andrew Reynolds <areynold@illinois.edu>
2020-02-12 16:36:06 -0600

    stub sitemap submitter addon

---
+++ README.md
@@ -0,0 +1,7 @@
+# ExpressionEngine Submit Sitemap Addon
+
+An ExpressionEngine addon that pings search engines with an updated sitemap when new content is published.
+
+## Dependencies
+
+- ExpressionEngine v3+
---
+++ constants.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace IllinoisPublicMedia\SubmitSitemap;
+
+if (!defined('BASEPATH')) { 
+    exit ('No direct script access allowed.');
+}
+
+class Constants 
+{
+    const NAME = 'Submit Sitemap';
+
+    const AUTHOR = 'Illinois Public Media';
+
+    const AUTHOR_URL = 'https://will.illinois.edu';
+
+    const DESCRIPTION = 'Pings search engines with updated sitemaps on save.';
+
+    const DOCS_URL = 'https://gitlab.engr.illinois.edu/willpublicmedia/expressionengine-submit-sitemap';
+    
+    const VERSION = '0.0.0';
+}
